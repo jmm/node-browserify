ae9aedba8b5dbcffc90aac2d49a1565b15160aae
Jesse McCarthy <git_commits@jessemccarthy.net>
2015-01-08 17:11:07 -0500

    Make multi_require test fail.

--- test/multi_require.js
+++ test/multi_require.js
@@ -6,7 +6,7 @@ test('require same file locally and globally', function (t) {
     t.plan(1);
 
     var b = browserify(__dirname + '/multi_require/main.js');
-    b.require('./multi_require/a.js', {expose: 'a'});
+    b.require('./multi_require/a', {expose: 'a'});
 
     b.bundle(function (err, src) {
         var c = {t: t};
